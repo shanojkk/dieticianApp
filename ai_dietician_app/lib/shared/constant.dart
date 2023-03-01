80b06a73f830c295a39204ddf923d18515d827de
Jason07311 <52784322+Jason07311@users.noreply.github.com>
2023-03-01 20:39:14 +0800

    Added new constant
    docs :  Added regex validation for height and weight, added bread crumb item

--- ai_dietician_app/lib/shared/constant.dart
+++ ai_dietician_app/lib/shared/constant.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_breadcrumb/flutter_breadcrumb.dart';
 
 const textInputDecoration = InputDecoration(
                   fillColor: Colors.white,
@@ -12,3 +13,30 @@ const textInputDecoration = InputDecoration(
                     borderSide: BorderSide(color: Colors.pink, width: 2.0)
                   )
                 );
+
+
+BreadCrumbItem breadCrumbItem = BreadCrumbItem(
+                    content: SizedBox(height: 7.0,width: 19.0),                  
+                    disableColor: Colors.black,
+                    border: BorderSide(color: Colors.black, width: 2.0),
+                    borderRadius: BorderRadius.circular(10.0),
+                    );              
+
+
+
+
+// Regular Expression
+
+// Validate height
+String? validateHeight(String value){
+  String height = r'(^([0-9]{3}$)';
+  RegExp regexHeight = new RegExp(height);
+  if(regexHeight.hasMatch(value)){
+    return 'Please enter valid mobile number';
+  }
+  else if(value.length == 0){
+    return 'Please enter mobile number';
+  }
+  return null;
+}
+
