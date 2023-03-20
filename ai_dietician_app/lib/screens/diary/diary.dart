5107b05157ae02a1e4de5aaee8f7d161e9c9b789
Jason07311 <52784322+Jason07311@users.noreply.github.com>
2023-03-20 19:52:50 +0800

    Style change and navigation to profile
    style: Changing the button of logout and developing button leading to profile with icon
    
    Navigation from diary to logout and profile are done

--- ai_dietician_app/lib/screens/diary/diary.dart
+++ ai_dietician_app/lib/screens/diary/diary.dart
@@ -1,3 +1,4 @@
+import 'package:ai_dietician_app/screens/profile/profile.dart';
 import 'package:ai_dietician_app/screens/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -10,24 +11,50 @@ class Diary extends StatefulWidget {
 }
 
 class _DiaryState extends State<Diary> {
+
   final AuthService _auth = AuthService();
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Diary"),
         actions: <Widget>[
-          ElevatedButton.icon(
+
+          // Log out
+          IconButton(
             onPressed:() async {
               await _auth.signOut();
             },
-            icon: Icon(Icons.logout), 
-            label: Text('logout'),
+            icon: Icon(Icons.logout),             
             style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.brown[400], 
-              elevation: 0.0
+              backgroundColor: Colors.transparent, 
+              elevation: 0.0,              
               ),
-            )
+            ),
+
+            // Profile
+            IconButton(
+              onPressed: () async {
+                // Lead to profile page 
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: ((context) => Profile()) 
+                )
+                );
+
+
+              },
+              icon: Icon(Icons.people),             
+              style: ElevatedButton.styleFrom(
+                elevation: 0.0,                
+              ),
+              
+
+
+            ),
+
+
         ],
       ),
 
